a39d064ba087feb1b29389d906e7bbe564439a51
Sammy Schneider <SammySchneider@Sammys-Air.attlocal.net>
2020-09-02 08:15:38 -0700

    cleaning up code for readablitity

--- lyrics_app/app.js
+++ lyrics_app/app.js
@@ -10,23 +10,27 @@ $(() => {
 
 
     var settings = {
-  	  "url": "https://genius.p.rapidapi.com/search?q=" + userInput,
-  	  "method": "GET"
+  	   "url": "https://genius.p.rapidapi.com/search?q=" + userInput,
+  	   "method": "GET",
+  	   "headers": {
+  		   "x-rapidapi-host": "genius.p.rapidapi.com",
+  	     "x-rapidapi-key": "bc0b35f5bdmshcbb8935a94c132fp1abf85jsnb66bf20c2ba9"
+  	    }
     }
 
     $.ajax(settings)
-      .then((data) => {
-        for (let i = 0;i<10;i++){
-          const songTitles = data.response.hits[i].result.title
-          const $h1 = $('<button>').addClass('song').text(songTitles).appendTo('.container')
+        .then((data) => {
+          for (let i = 0;i<10;i++){
+            const songTitles = data.response.hits[i].result.title
+            const $h1 = $('<button>').addClass('song').text(songTitles).appendTo('.container')
     }
-      $('.song').on('click', (event) => {
+    $('.song').on('click', (event) => {
         const songTitle = $(event.target).text();
         $.ajax({
-            url:'https://orion.apiseeds.com/api/music/lyric/' +userInput + '/' + songTitle + '?apikey=XTym7nXEaPHH63xM5ogFCgj1kKiAu0EppXgcwlSA0fktKSn3krwKrJIOmTAnZSZZ'
+              url:'https://orion.apiseeds.com/api/music/lyric/' +userInput + '/' + songTitle + '?apikey=XTym7nXEaPHH63xM5ogFCgj1kKiAu0EppXgcwlSA0fktKSn3krwKrJIOmTAnZSZZ'
         }).then(
             (data)=>{
-              const $songLrics = data.result.track.text
+            const $songLrics = data.result.track.text
               $('.lyrics').append($songLrics)
               $('#modal').css('display','block')
             },
@@ -34,7 +38,7 @@ $(() => {
                 console.log('bad');
             }
         );
-    })
+      })
 
     });
 
